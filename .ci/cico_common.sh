e901aca30529cc8809a44fe690fcb52a45ef8ac2
Vitalii Parfonov <vparfono@redhat.com>
2019-12-12 11:19:12 +0200

    Try to fix deploy command
    Signed-off-by: Vitalii Parfonov <vparfono@redhat.com>

--- .ci/cico_common.sh
+++ .ci/cico_common.sh
@@ -51,7 +51,7 @@ build_and_deploy_artifacts() {
     if [ $? -eq 0 ]; then
         echo 'Build Success!'
         echo 'Going to deploy artifacts'
-        scl enable rh-maven33 "mvn clean deploy -Pcodenvy-release -DcreateChecksum=true  -Dgpg.passphrase=$CHE_OSS_SONATYPE_PASSPHRASE"
+        scl enable rh-maven33 "mvn clean deploy -DcreateChecksum=true  -Dgpg.passphrase=$CHE_OSS_SONATYPE_PASSPHRASE"
         exit 0
     else
         echo 'Build Failed!'
