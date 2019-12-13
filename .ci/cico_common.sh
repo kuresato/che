ef107f343babb22b9c7deead2a5db22465bb5a47
Vitalii Parfonov <vparfono@redhat.com>
2019-12-13 15:16:18 +0200

    test commit
    Signed-off-by: Vitalii Parfonov <vparfono@redhat.com>

--- .ci/cico_common.sh
+++ .ci/cico_common.sh
@@ -62,7 +62,7 @@ build_and_deploy_artifacts() {
 }
 
 releaseProject() {
-    #test 3
+    #test 4
     CUR_VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
     TAG = $(echo $CUR_VERSION | cut -d'-' -f1) #cut SNAPSHOT form the version name
     echo -e "\x1B[92m############### Release: $TAG\x1B[0m"
